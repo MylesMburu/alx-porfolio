d437fc26c1e92b5f70fa454da5c1493c139ee2e8
MylesMburu <ngicumyles@gmail.com>
2023-01-27 19:12:32 +0300

    home style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './header.css'
-import CTA from './CTA'
+import CV from "../../assets/wanjapi/Myles Mburu Ngicu.pdf"
 import ME from '../../assets/wanjapi/me.avatar.png'
 
 const Header = () => {
   return (
     <header id='header'>
       <div className="container header__container">
+        <div className="hero">
         <h5>Hey there, I'm</h5>
         <h1>Myles Mburu</h1>
         <h5 className='text-light'>Full Stack Developer</h5>
-        <CTA/>
+        <div className='cta'>
+        <a href={CV} download className='btn btn-primary'>Download CV</a>
+    </div>
+        </div>
 
         <div className='me'>
           <img src={ME} alt="" />
