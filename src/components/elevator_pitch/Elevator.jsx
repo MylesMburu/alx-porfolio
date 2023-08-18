562596732632cf54763adb6467571da5e51fcf48
MylesMburu <ngicumyles@gmail.com>
2023-08-18 12:59:55 +0300

    Added elevator pitch

--- src/components/elevator_pitch/Elevator.jsx
+++ src/components/elevator_pitch/Elevator.jsx
@@ -1,21 +1,17 @@
-import React from 'react'
-import './elevator.css'
+import React from 'react';
+import './elevator.css';
 
 export const Elevator = () => {
   return (
     <section className='elevator-container'>
-        <h2>Elevator Pitch</h2>
-        <div>
-        <iframe 
-              width="560"
-              height="315" 
-              src="https://www.youtube.com/embed/VIDEO_ID" 
-              title="YouTube video player" 
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen>
-              </iframe>
-        </div>
+      <h2>Elevator Pitch</h2>
+      <div>
+          <iframe 
+          width="560" height="315" 
+          src="https://www.youtube.com/embed/aTCpqwhNQzs" title="YouTube video player" 
+          frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+          </iframe>
+      </div>
     </section>
   )
 }
